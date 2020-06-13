4d314f961f2b77e265875a25e70339477bce6397
Leda <ledacamposfigueiredo@hotmail.com>
2020-06-13 20:11:30 -0300

    Update DriverFactory.java

--- Selenium-Java-Cucumber-master/src/test/java/automacao/DriverFactory.java
+++ Selenium-Java-Cucumber-master/src/test/java/automacao/DriverFactory.java
@@ -16,7 +16,7 @@ public class DriverFactory {
 	}
 
 	private static void createDriver() {
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		//driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
