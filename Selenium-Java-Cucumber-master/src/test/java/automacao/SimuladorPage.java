1679c235c8f817c4c4d41ba6f164b41d6fd0c48c
Leda <ledacamposfigueiredo@hotmail.com>
2020-06-16 19:18:37 -0300

    Update SimuladorPage.java

--- Selenium-Java-Cucumber-master/src/test/java/automacao/SimuladorPage.java
+++ Selenium-Java-Cucumber-master/src/test/java/automacao/SimuladorPage.java
@@ -2,38 +2,44 @@ package automacao;
 
 import org.openqa.selenium.By;
 
-public class SimuladorPage extends SimuladorBasePage{
-	
-	
+public class SimuladorPage extends SimuladorBasePage {
+
 	public void acessarTelaPrincipal() {
 		acessar(BaseURL);
 	}
-	
-	public void preencherCamposDatela(String valorAplicar, String valorInvestir, String tempo, String blocoResultadoSimulacao) {
-		escrever(By.id("valorAplicar"),valorAplicar);
-		escrever(By.id("valorInvestir"),valorInvestir);
-		escrever(By.id("tempo"),tempo);
-		
+
+	public void preencherCamposDatela(String valorAplicar, String valorInvestir, String tempo,
+			String blocoResultadoSimulacao) {
+		escrever(By.id("valorAplicar"), valorAplicar);
+		escrever(By.id("valorInvestir"), valorInvestir);
+		escrever(By.id("tempo"), tempo);
+
 	}
-	
+
 	public void preencherCampoValorAplicar(String valorAplicar) {
-		escrever(By.id("valorAplicar"),valorAplicar);
+		escrever(By.id("valorAplicar"), valorAplicar);
 	}
-	
+
 	public void preencherCampoValorInvestir(String valorInvestir) {
-		escrever(By.id("valorInvestir"),valorInvestir);
+		escrever(By.id("valorInvestir"), valorInvestir);
 	}
-	
+
 	public void preencherCampoTempo(String tempo) {
-		escrever(By.id("tempo"),tempo);
+		escrever(By.id("tempo"), tempo);
 	}
 
 	public void cliqueBotaoSimular() {
-		clique(By.className("btn btnAmarelo btnSimular"));
+		clique(By.xpath("//button[@class='btn btnAmarelo btnSimular']"));
 	}
 
 	public String getMensagem() {
 		return obterTexto(By.className("blocoResultadoSimulacao"));
 	}
-	
+
+	public void cliqueBotaoRefazerSimulacao() {
+		clique(By.xpath("//button[@class='btn btnAmarelo btnRefazer']"));
+		//class="btn btnAmarelo btnRefazer"
+
+	}
+
 }
